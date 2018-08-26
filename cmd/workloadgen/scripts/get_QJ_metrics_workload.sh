5519a949ef473ca92aa3151041c564b87736592e
Stefania Costache <svcostac@us.ibm.com>
2018-08-26 15:25:10 -0500

    plot script changes

--- cmd/workloadgen/scripts/get_QJ_metrics_workload.sh
+++ cmd/workloadgen/scripts/get_QJ_metrics_workload.sh
@@ -11,6 +11,7 @@ mkdir ${file}-dir
 grep Allocated $file | tr -s ' ' | cut -d ' ' -f 1,6,8 > ${file}-dir/alloc.cvs
 
 grep PendingQueue $file | tr -s ' ' | cut -d ' ' -f 5,6 > ${file}-dir/queues.cvs
+sed -i -e 's/=/ /g' ${file}-dir/queues.cvs ; cat ${file}-dir/queues.cvs | cut -d ' ' -f 2,4 > ${file}-dir/queue_numbers.cvs
 
 grep DeclaredCompletion $file > ${file}-dir/stats.txt
 cat ${file}-dir/stats.txt | cut -d ' ' -f 2,4 | cut -d '-' -f 2 | sort -u -n -k1 > ${file}-dir/delay.cvs
